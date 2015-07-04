bf36ee0b221d2e4d4c12f9d7f0d9c52664a8f096
ambs <ambs@cpan.org>
2015-07-04 17:58:56 +0100

    First draft version

--- lib/DB_File/Utils.pm
+++ lib/DB_File/Utils.pm
@@ -1,5 +1,24 @@
-use strict;
-use warnings;
+# ABSTRACT: Creates db_util command line for DB_File management
+
 package DB_File::Utils;
+use App::Cmd::Setup -app;
 
 1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+DB_File::Utils - main module for db_util command line tool
+
+=head1 DESCRIPTION
+
+Please refer to C<perldoc db_util> for detail on module usage.
+
+=head1 SEE ALSO
+
+db_util (3)
+
+=head1 AUTHOR
+
+Alberto Simões C<< <ambs@cpan.org> >>
